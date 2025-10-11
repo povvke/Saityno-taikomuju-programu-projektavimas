5af4fc649287e8c35de9e0e66682bd939975aa3d
povvke <markeviciusp@yahoo.com>
2025-10-12 00:08:26 +0300

    crud recipes

--- server/main.py
+++ server/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from .models import create_db_and_tables
 from .routes.categories import router as categories_router
+from .routes.recipes import router as recipes_router
 
 app = FastAPI()
 
@@ -12,3 +13,4 @@ def on_startup():
 
 
 app.include_router(categories_router, prefix="/categories", tags=["categories"])
+app.include_router(recipes_router, prefix="/recipes", tags=["recipes"])
